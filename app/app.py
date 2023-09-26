1e7bf992a643e26d1f2715b91ce6460d9fee134d
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2023-09-26 11:44:13 +0100

    added logging and exception handling

--- app/app.py
+++ app/app.py
@@ -1,12 +1,35 @@
 import random
+import logging
+
+# Configure logging
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
+
+logger = logging.getLogger()
 
 
 def generate_random_answer():
+    try:
+        # List of possible answers
+        answers = ["yes", "no", "maybe"]
 
-    # List of possible answers
-    answers = ["yes", "no", "maybe"]
+        # Generate a random answer
+        random_answer = random.choice(answers)
 
-    # Generate a random answer
-    random_answer = random.choice(answers)
+        logger.info(f"Generated random answer: {random_answer}")
 
-    return random_answer
+        return {
+            "statusCode": 200,
+            "body": random_answer
+        }        
+    
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": "Internal Server Error"
+        }       
+
